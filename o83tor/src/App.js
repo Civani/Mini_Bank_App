6b9fbccb2291002109ce6c7c9064d842b42e357a
Shivani Tiwari <104130058+Civani@users.noreply.github.com>
2022-12-28 21:47:18 +0530

    Add files via upload

---
+++ o83tor/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import "./styles.css";
+import { Authenticate } from "./components/Authenticate";
+
+function App() {
+  return <Authenticate />;
+}
+
+export default App;
